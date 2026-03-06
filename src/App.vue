9dd03ffcb526214b75df65c6049d15bd773b3b4e
Adeoti Opeyemi <adeotijamiu95@gmail.com>
2026-03-06 13:39:04 +0100

    i added some component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vueeeee" />
+ <Service/>
+  <About/>
+  <People/>
 </template>
 
+
+
+
+
+<script>
+import Service from './components/Service.vue';
+import About from './components/About.vue';
+import People from './components/People.vue';
+  export default {
+    components:{
+      About,
+      People,Service
+    }
+  }
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
