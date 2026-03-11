ebf2b8df105863bd6f74d1074223c9af343aa767
Islamiyyah-S <shittumiyah03@gmail.com>
2026-03-11 10:09:12 +0100

    Edited landing.vue

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -9,7 +9,6 @@
       <LatestPost/>
       <Home />
       <Footer/>
-      <Sidebar/>
     </div>
 </template>
 
@@ -24,7 +23,6 @@ import DiscoverLoans from '../components/DiscoverLoans.vue';
 import CustomerSays from '../components/CustomerSays.vue';
 import LatestPost from '../components/LatestPost.vue';
 import People from '../components/People.vue';
-import Sidebar from '../dashboard/Sidebar.vue';
 
 
 export default {
@@ -39,7 +37,6 @@ export default {
     CustomerSays,
     LatestPost,
     People,
-    Sidebar
   }
 }
 </script>
